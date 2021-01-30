bc51f9590f3ed9f2a28ad006586223b54242b21e
[iamrukafeso] <[iamrukafeso@gmail.com]>
2021-01-30 06:39:12 +0000

    Police profile XML

--- app/src/main/java/com/rukayat_oyefeso/police_tracking_information/policeProfile.java
+++ app/src/main/java/com/rukayat_oyefeso/police_tracking_information/policeProfile.java
@@ -1,14 +1,180 @@
 package com.rukayat_oyefeso.police_tracking_information;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class policeProfile extends AppCompatActivity {
+
+    private Button mUpdateBtn,mChangeProfileImgBtn;
+    private CircleImageView mProfileImage;
+    private static final int IMAGE_PICK = 1;
+
+    DrawerLayout drawerLayout;
+    Switch switchNightMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_police_profile);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //Assign variable for night or dark mode
+        drawerLayout = findViewById(R.id.drawer_layout);
+        switchNightMode = findViewById(R.id.switchNightMode);
+
+        int currentMode = AppCompatDelegate.getDefaultNightMode();
+
+        if (currentMode == AppCompatDelegate.MODE_NIGHT_YES){
+            switchNightMode.setChecked(true);
+        } else {
+            switchNightMode.setChecked(false);
+        }
+
+        switchNightMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+                restartCurrentActivity();
+            }
+        });
+
+    }
+
+    //night mode
+    private void restartCurrentActivity(){
+        Intent intent = getIntent();
+        finish();
+        startActivity(intent);
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+    }
+
+    public void ClickMenu(View view){
+        //open drawer
+        openDrawer(drawerLayout);
+    }
+
+    private static void openDrawer(DrawerLayout drawerLayout) {
+        //Open drawer layout
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    public void ClickLogo(View view){
+        //Close drawer
+        closeDrawer(drawerLayout);
+    }
+
+    private static void closeDrawer(DrawerLayout drawerLayout) {
+        //close drawer layout
+        //check condition
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            //When drawer is open
+            //close drawer
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    public void ClickHome(View view){
+        //Redirect activity to detect text
+        redirectActivity(this, MainActivity.class);
+    }
+
+    public void ClickUserProfile(View view){
+        //Recreate activity
+        recreate();
+    }
+
+    public void ClickDetectText(View view){
+        //Redirect activity to Scanner activity
+        redirectActivity(this, TextRecognizer.class);
+    }
+
+    public void ClickCrimeNews(View view){
+        //Redirect activity to crime news
+        redirectActivity(this, CrimeNews.class);
+    }
+
+    public void ClickSettings(View view){
+        //Redirect activity to settings activity
+        redirectActivity(this, Settings.class);
+    }
+
+    public void ClickAbout(View view){
+        //Redirect activity to about
+        redirectActivity(this, About.class);
+    }
+
+    public void ClickHelp(View view){
+        //Redirect activity to help
+        redirectActivity(this, Help.class);
+    }
+
+    public void ClickLogOut(View view){
+        //Redirect activity to log out
+        logout(this);
+    }
+
+    private static void logout(final Activity activity){
+        //Initialize alert dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        //Set title
+        builder.setTitle("Logout");
+        //Set message
+        builder.setMessage("Are you sure you want to logout ?");
+        //positive yes button
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Finish activity
+                activity.finishAffinity();
+                //Exit app
+                System.exit(0);
+            }
+        });
+        //Negative no button
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Dismiss dialog
+                dialog.dismiss();
+            }
+        });
+        //show dialog
+        builder.show();
+    }
+
+    private static void redirectActivity(Activity activity, Class aClass) {
+        //Initialize intent
+        Intent intent = new Intent(activity, aClass);
+        //set flag
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //start activity
+        activity.startActivity(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //Close drawer
+        closeDrawer(drawerLayout);
     }
 }
